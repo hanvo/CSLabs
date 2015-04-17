44ab13f635eac82259c885034cfe3c1195e563ab
Don Phan <phand@purdue.edu>
2015-04-16 20:34:42 -0400

    Encrypt func works

--- Project4/src/Project4.java
+++ Project4/src/Project4.java
@@ -1,20 +1,51 @@
-/**
- * 
- */
 
-/**
- * @author phand
- *
- */
 public class Project4 {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World,");
+	public static String VigEncrypt(String plain, String key){
+		String kprime = "";
+		String cipher = "";
+		int x;
+		
+		plain = plain.toUpperCase();
+		key = key.toUpperCase();
+		
+		int plainLen = plain.length();
+		int keyLen = key.length();
+		
+		int division = plainLen / keyLen;
+		
+		for(x = 0; x < division; x++){
+			kprime += key;
+		}
+		
+		if(plainLen != kprime.length()){
+			int difference = plainLen - (x * keyLen);
+			for(x = 0; x < difference; x++){
+				kprime += key.charAt(x);
+			}
+		}
 
+		for(x = 0; x < plainLen; x++){
+			int plainCode = (char)plain.charAt(x);
+			int kprimeCode = (char)kprime.charAt(x);
+			
+			int plainAlphPos = plainCode - 65;
+			int kprimeAlphaPos = kprimeCode - 65;
+
+			int newCodeAlphaPos = plainAlphPos + kprimeAlphaPos;
+			newCodeAlphaPos = Math.abs(newCodeAlphaPos % 26);
+			
+			cipher += Character.toString((char)(newCodeAlphaPos + 65));
+		}
+		return cipher;
+	}
+	
+	public String VigDecrypt(String Cipher, String Key){
+		return "";
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(VigEncrypt("databases","abcde"));
 	}
 
 }
