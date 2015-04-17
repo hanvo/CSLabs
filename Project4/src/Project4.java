e9c2b70d93261f4b505bc1d021f0283b811d8261
Don Phan <phand@purdue.edu>
2015-04-17 12:57:49 -0400

    Vig Decrypts

--- Project4/src/Project4.java
+++ Project4/src/Project4.java
@@ -40,12 +40,55 @@ public class Project4 {
 		return cipher;
 	}
 	
-	public String VigDecrypt(String Cipher, String Key){
-		return "";
+	public static String VigDecrypt(String cipher, String key){
+		String kprime = "";
+		String plain = "";
+		int x;
+		int check;
+
+		
+		cipher = cipher.toUpperCase();
+		key = key.toUpperCase();
+		
+		int cipherLen = cipher.length();
+		int keyLen = key.length();
+		
+		int division = cipherLen / keyLen;
+		
+		for(x = 0; x < division; x++){
+			kprime += key;
+		}
+		
+		if(cipherLen != kprime.length()){
+			int difference = cipherLen - (x * keyLen);
+			for(x = 0; x < difference; x++){
+				kprime += key.charAt(x);
+			}
+		}
+
+		for(x = 0; x < cipherLen; x++){
+			int cipherCode = (char)cipher.charAt(x);
+			int kprimeCode = (char)kprime.charAt(x);
+			
+			int cipherAlphPos = cipherCode - 65;
+			int kprimeAlphaPos = kprimeCode - 65;
+
+			int plainAlphaPos = cipherAlphPos - kprimeAlphaPos;
+			if((check = cipherAlphPos - kprimeAlphaPos) < 0){
+				check = Math.abs(check);
+				plainAlphaPos = 26 - check;
+			} else {
+				plainAlphaPos = Math.abs(plainAlphaPos % 26);
+			}
+			
+			plain += Character.toString((char)(plainAlphaPos + 65));
+		}
+		return plain;
 	}
 	
 	public static void main(String[] args) {
-		System.out.println(VigEncrypt("databases","abcde"));
+		System.out.println(VigEncrypt("BEWARETHEJABBERWOCKMYSONTHEJAWSTHATBITETHECLAWSTHATCATCH","VIGENERECIPHER"));
+		System.out.println(VigDecrypt("WMCEEIKLGRPIFVMEUGXQPWQVIOIAVEYXUEKFKBTALVXTGAFXYEVKPAGY", "VIGENERECIPHER"));
 	}
 
 }
